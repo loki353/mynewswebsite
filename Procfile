0f5b9a3594cada40ad8f4a456bc608be5200379b
Lokesh B <lokeshbakkolaa@gmail.com>
2025-10-11 17:11:10 +0530

    Add start.sh and Procfile for Railway deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: java -cp bin:lib/gson-2.10.1.jar NewsWebApp
+web: ./start.sh
+
---
+++ start.sh
@@ -0,0 +1,3 @@
+javac -cp "lib/gson-2.10.1.jar" -d bin src/NewsWebApp.java
+
+java -cp "bin:lib/gson-2.10.1.jar" NewsWebApp
